e1ab529e487d193cee2f19bc1baf3091e35f21d7
Augustus Rossi <147848584+Augustusrossi@users.noreply.github.com>
2026-09-10 22:20:26 -0300

    feat: first version of v1 library project

---
+++ main.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#define MAX_TITULOS 3
+
+int main()
+{
+    int codigos[MAX_TITULOS] = {0};
+    int estoques[MAX_TITULOS] = {0};
+    int qtdExemplares = 0;
+    int codBusca;
+    int posBusca = -1;
+
+    int qtdTitulos = 0;//contador
+
+    
+
+    
+    for(qtdTitulos = 0; qtdTitulos < 3; qtdTitulos++){
+        printf("\nDigite o codigo do novo livro: ");
+        scanf("%d", &codigos[qtdTitulos]);
+        if(codigos[qtdTitulos] < 0){
+            do{
+                printf("Codigo invalido. Digite um valor maior ou igual a zero.\n");
+                printf("\nDigite o codigo do novo livro: ");
+                scanf("%d", &codigos[qtdTitulos]);
+            }while (codigos[qtdTitulos] < 0);
+        }
+        
+        for(int j = 1; j <= qtdTitulos-1; j++){
+            if(codigos[qtdTitulos] == codigos[j]){
+                do{
+                    printf("Codigo invalido. Digite um valor diferente de um codigo ja inserido anteriormente.\n");
+                    printf("\nDigite o codigo do novo livro: ");
+                    scanf("%d", &codigos[qtdTitulos]);
+                }while (codigos[qtdTitulos] == codigos[j]);
+            }
+        }
+        
+        printf("\nDigite a quantidade de exemplares em estoque: ");
+        scanf("%d", &estoques[qtdTitulos]);
+        
+        if(estoques[qtdTitulos] < 0){
+            do{
+                printf("Quantidade de exemplares invalida. Digite um valor maior ou igual a zero.\n");
+                printf("\nDigite a quantidade de exemplares em estoque: ");
+                scanf("%d", &estoques[qtdTitulos]);
+            }while (estoques[qtdTitulos] < 0);
+        }
+        
+        
+        qtdExemplares += estoques[qtdTitulos];
+    }
+    
+    for(int y = 0; y < 3; y++){
+        printf("\nLivro - codigo: %d possui %d exemplares em estoque.", codigos[y], estoques[y]);
+    }
+    
+    printf("\nO total de livros no sistema eh: %d", qtdExemplares);
+    
+    
+    do{
+        printf("\nDigite um codigo de livro para busca: ");
+        scanf("%d", &codBusca);
+    } while(codBusca < 0);
+    
+    for(int x = 0; x < 3; x++){
+        if(codBusca == codigos[x])
+            posBusca = x;
+            if(estoques[posBusca] == 0)
+                printf("\ntitulo temporariamente indisponivel");
+            else
+                printf("\ntitulo disponivel com %d em estoque", estoques[posBusca]);
+    }
+    if(posBusca == -1){
+        printf("\nNao possuimos este titulo no nosso acervo!");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
